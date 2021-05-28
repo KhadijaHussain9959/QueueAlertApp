07f73bc93dcc253f2d3915e9b3ba1af01bdf2c4f
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2021-05-28 10:12:34 +0500

    architecture changed

--- App.js
+++ App.js
@@ -1,47 +1,17 @@
-/* eslint-disable react-native/no-inline-styles */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 import 'react-native-gesture-handler';
-import React, {useEffect, useState} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-// import {SafeAreaView, StatusBar} from 'react-native';
-import SplashScreen from 'react-native-splash-screen';
-import Home from './src/screens/Home/Home.js';
-import Login from './src/screens/Login/Login.js';
-import SignUp from './src/screens/SignUp/SignUp.js';
+import NavigationContainer from './src/navigation';
+import React, {useEffect} from 'react';
 
-const Stack = createStackNavigator();
+import {LogBox} from 'react-native';
 
-const App: () => React$Node = () => {
+const App = () => {
   useEffect(() => {
-    SplashScreen.hide();
+    LogBox.ignoreAllLogs();
   }, []);
-  // const [User, setUser] = useState({
-  //   name: 'haseeb',
-  //   email: 'abc@gmail.com',
-  //   userid: '1',
-  // });
 
-  const [User, setUser] = useState(null);
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {User ? (
-          <Stack.Screen name="Home" component={Home} />
-        ) : (
-          <>
-            <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="Login" component={Login} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NavigationContainer />
   );
 };
+
 export default App;
